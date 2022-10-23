36fe2bff3a3d259b68c9d4d1068a4789023b97a1
ckstn0777 <ckstn0777@naver.com>
2022-10-23 21:57:25 +0900

    feat: color 디자인 가이드 만들기

---
+++ src/styles/color/color.stories.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { css } from '@emotion/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { lightColorTheme } from './palette'
+
+export default {
+  title: 'Design System/Color',
+  component: () => null,
+} as ComponentMeta<any>
+
+const Color = ({ name, color }: { name: string; color: string }) => (
+  <div css={colorWrapper}>
+    <div css={colorBox(color)}></div>
+    <b>{name}</b>
+    <p>{color}</p>
+  </div>
+)
+
+const ColorList = ({
+  colors,
+  namespace,
+}: {
+  colors: Record<string, string>
+  namespace: string
+}) => (
+  <>
+    <h3>{namespace}</h3>
+    <div css={colorRowWrapper}>
+      {Object.entries(colors)
+        .filter(([key]) => key.includes(namespace))
+        .map(([key, value]) => (
+          <Color key={key} name={key} color={value} />
+        ))}
+    </div>
+  </>
+)
+
+const Template: ComponentStory<any> = () => {
+  const colors = lightColorTheme
+
+  return (
+    <div css={wrapper}>
+      <h1>Design System : Color</h1>
+      <section>
+        <h2>Color</h2>
+        <ColorList colors={colors} namespace="accent" />
+        <ColorList colors={colors} namespace="primary" />
+      </section>
+    </div>
+  )
+}
+
+export const Default = Template.bind({})
+
+const wrapper = css`
+  display: flex;
+  flex-direction: column;
+  padding: 1.6rem;
+
+  background-color: #262626;
+  color: white;
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 700;
+  }
+`
+
+const colorRowWrapper = css`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+`
+
+const colorWrapper = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const colorBox = (color: string) => css`
+  width: 4rem;
+  height: 4rem;
+  border-radius: 0.4rem;
+  background-color: ${color};
+`
---
+++ src/styles/color/palette.ts
@@ -0,0 +1,41 @@
+export const lightColorTheme = {
+  /** Background Color */
+  'accent-0': '#ffffff',
+  'accent-1': '#fafafa',
+  'accent-2': '#f5f5f5',
+  'accent-3': '#eeeeee',
+  'accent-4': '#e0e0e0',
+  'accent-5': '#9e9e9e',
+  'accent-6': '#757575',
+  'accent-7': '#616161',
+  'accent-8': '#424242',
+  'accent-9': '#212121',
+
+  /** Brand Color */
+  primary: '#009688',
+  'primary-hover': '#007e72',
+  'primary-active': '#007267',
+
+  /** Status Color */
+}
+
+export const darkColorTheme = {
+  /** Background Color */
+  'accent-0': '#1b1b1b',
+  'accent-1': '#020202',
+  'accent-2': '#2e2e2e',
+  'accent-3': '#3d3d3d',
+  'accent-4': '#5b5b5b',
+  'accent-5': '#848484',
+  'accent-6': '#979797',
+  'accent-7': '#b3b3b3',
+  'accent-8': '#bdbdbd',
+  'accent-9': '#e8eaed',
+
+  /** Brand Color */
+  primary: '#73e6db',
+  'primary-hover': '#61d9ce',
+  'primary-active': '#4eccc0',
+
+  /** Status Color */
+}
