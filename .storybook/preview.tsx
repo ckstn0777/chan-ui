9d287d1a3cf569bde720dde81ed0790e5c83f19f
ckstn0777 <ckstn0777@naver.com>
2022-10-28 21:26:49 +0900

    feat: storybook dark mode 추가

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,6 @@
+import React from 'react'
 import { ChanProvider } from '../src/context/ChanProvider'
+import { themes } from '@storybook/theming'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -7,6 +9,12 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  darkMode: {
+    // Override the default dark theme
+    dark: { ...themes.dark, appContentBg: '#333333', appBg: '#2f2f2f' },
+    // Override the default light theme
+    light: { ...themes.normal },
   },
 }
 
